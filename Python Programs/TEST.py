02354ec961f9bdc883d6249b186e9886b9157d52
Kuldeep Dhiman <81000487+KuldeepXD@users.noreply.github.com>
2021-06-14 07:56:27 +0530

    Delete Python Programs directory

--- Python Programs/TEST.py
+++
@@ -1,3 +0,0 @@
-character_name = 'john'
-age2= '9'
-print( character_name + " and he is " + age2 )
--- Python Programs/database!1.py
+++
@@ -1,5 +0,0 @@
-######  15/03/2021   #####
-#ER Diagram
-#mysql pass rkhna hai root
-#RDBMS hai ER diagram(rekation bnata hai)
-#RDBMS ki jroorat pdi relation bnane ke lie
